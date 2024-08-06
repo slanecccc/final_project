9e716adecf76c905e81f7ebd1117ed5f1416c48a
Goryachev K.S. <google.kost@yandex.ru>
2024-08-07 00:57:12 +0300

    Реализовал отображение приложения main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+def main(request: HttpRequest):
+    return render(request, 'main.html')
